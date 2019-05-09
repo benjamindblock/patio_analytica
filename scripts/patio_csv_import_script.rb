6229ee609d4465175d8b48584ea4487ea52541dd
Ben Block <benblock@Bens-MacBook-Pro.local>
2019-05-08 20:09:05 -0700

    updating script and removing weather for now

--- scripts/patio_csv_import_script.rb
+++ scripts/patio_csv_import_script.rb
@@ -1,6 +1,6 @@
 require 'patio_csv_importer'
 require 'CSV'
 
-filepath = Rails.root.join('tmp', 'import.csv')
+filepath = Rails.root.join('tmp', 'import_files', 'import.csv')
 importer = PatioCsvImporter.new(filepath)
 importer.import_csv
